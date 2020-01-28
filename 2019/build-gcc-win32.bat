40d91f1aa9279fec2add9023f820fcd5923ff6b2
acoto87 <acoto87@gmail.com>
2020-01-28 00:09:53 -0600

    Added day 6 solution

--- 2019/build-gcc-win32.bat
+++ 2019/build-gcc-win32.bat
@@ -5,7 +5,7 @@ SET OutputPath=.\build
 
 REM Compiler flags
 SET OptimizeFlags="-O2"
-REM SET DebugFlags="-g -O1"
+REM SET DebugFlags="-g"
 SET CommonCompilerFlags=-std=c99 -Wall -x c %OptimizeFlags% %DebugFlags%
 
 REM Create output path if doesn't exists
@@ -21,5 +21,6 @@ gcc %CommonCompilerFlags% ..\2.c -o 2.exe
 gcc %CommonCompilerFlags% ..\3.c -o 3.exe
 gcc %CommonCompilerFlags% ..\4.c -o 4.exe
 gcc %CommonCompilerFlags% ..\5.c -o 5.exe
+gcc %CommonCompilerFlags% ..\6.c -o 6.exe
 
 POPD
